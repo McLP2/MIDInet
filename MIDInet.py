4a3820908ba5f18b27a707395b2b899fb9de5695
McLP <wdv50jkc@gmail.com>
2018-08-17 17:42:56 +0200

    Fixed some general issues: - files were unloadable due to a typo - added TODO for multiple training files - added more comments - removed TimeDistribution (still does not work)

--- MIDInet.py
+++ MIDInet.py
@@ -6,22 +6,24 @@ from os import listdir
 
 # import midis
 
-read_from = 'midi_arrays'
+read_from = 'midi_arrays/'
 
+# TODO: figure out how this could work with multiple files
 midis = []
 midis_y = []
 # go through folder and add all arrays
-for file_name in listdir(read_from)[:10]:
+for file_name in listdir(read_from):
     arr = np.load(read_from + file_name)
     arr = arr[arr.keys()[0]]
     midis.append(np.reshape(arr[:arr.shape[0] - 1], (-1, 128, 1)))
-for file_name in listdir(read_from)[:10]:
+for file_name in listdir(read_from):
     arr = np.load(read_from + file_name)
     arr = arr[arr.keys()[0]]
     midis_y.append(np.reshape(arr[1:], (-1, 128, 1)))
 
 midi, midi_y = 0, 0
 
+# load just the first midi
 for file_name in listdir(read_from)[:1]:
     arr = np.load(read_from + file_name)
     arr = arr[arr.keys()[0]]
@@ -37,10 +39,10 @@ model = keras.models.Sequential()
 
 model.add(InputLayer(input_shape=(128, 1)))
 model.add(LSTM(1024, activation='relu', dropout=0.2, recurrent_dropout=0.2))
-model.add(TimeDistributed(Flatten()))
-model.add(TimeDistributed(Dense(256, activation='relu')))
+# model.add(TimeDistributed(Flatten()))
+model.add(Dense(256, activation='relu'))
 # model.add(TimeDistributed(Dropout(rate=0.2)))
-model.add(TimeDistributed(Dense(128, activation='softmax')))
+model.add(Dense(128, activation='softmax'))
 
 model.compile(optimizer=keras.optimizers.Adam(lr=0.001, beta_1=0 - 9, beta_2=0.999, epsilon=None),
               loss=keras.losses.binary_crossentropy,
